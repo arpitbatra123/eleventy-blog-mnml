ca3cebba8b1fda13138d1bc924330c098ac01e9f
Arpit Batra <arpitbatra123@gmail.com>
2021-03-04 19:41:26 +0530

    self host google fonts

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy('favicon.ico');
+  eleventyConfig.addPassthroughCopy('assets/fonts');
   const options = {
     html: true,
     breaks: true,
